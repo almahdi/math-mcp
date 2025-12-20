d10b374422e1ecc13fc64219e87237a59443ca0e
Ali Almahdi <ali@almahdi.cc>
2025-12-20 18:36:09 +0300

    Add Effect Platform dependencies and implement basic routing in the worker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { HttpRouter, HttpServerResponse, HttpApp } from "@effect/platform"
+
+const router = HttpRouter.empty.pipe(
+  HttpRouter.get("/", HttpServerResponse.text("Hello World from Effect Platform!")),
+  HttpRouter.all("*", HttpServerResponse.empty({ status: 404 })),
+  HttpRouter.catchAll(e => HttpServerResponse.text(`Internal Server Error: ${e}`, { status: 500 })),
+  HttpApp.toWebHandler
+)
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// return new Response('Hello World!');
+		return await router(request);
 	},
 } satisfies ExportedHandler<Env>;
